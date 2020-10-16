98325fe8125779a12aad66f29b1e105456b4f22b
Fernando Porazzi <fernandoporazzi@gmail.com>
2020-10-16 16:36:12 +0200

    Create function to reverse slice of strings

---
+++ helpers/reverse.go
@@ -0,0 +1,10 @@
+package helpers
+
+// Reverse takes an slice of any type and return
+func Reverse(s []string) []string {
+	for i, j := 0, len(s)-1; i < j; i, j = i+1, j-1 {
+		s[i], s[j] = s[j], s[i]
+	}
+
+	return s
+}
